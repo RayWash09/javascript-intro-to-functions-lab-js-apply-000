168b8f4cd60229f512e6e3f05a1e163faf355bfe
Raymond Washington <rtwashington90@gmail.com>
2020-02-18 16:13:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ function shout(string) {
   
   function whisper(string) {
     return string.toLowerCase()
-    }
+  }
   
     function logShout(string) {
       console.log('HELLO'); string.toUpperCase()
-      }
+    }
     
       function logWhisper(string) {
         console.log('hello'); string.toLowerCase()
-        }
+      }
         
-        function sayHiToGrandma
+        function sayHiToGrandma(string) {
+          
+        }
     
