3297b0aa37acdc00895e55f4f2ab85f356f23a7c
Raymond Washington <rtwashington90@gmail.com>
2020-02-18 16:35:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function shout(string) {
           if (string == string.toLowerCase)
             return "I can\'t hear you!"
             if (string == string.toUpperCase)
-              return "YES INDEED"
+              return "YES INDEED!"
               if (string == "I love you, Grandma.")
               return "I love you, too."
         }
